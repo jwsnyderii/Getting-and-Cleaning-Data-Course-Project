03861643c1abb69b7007b3db74ca5e759a8de851
jsnyder <jwsnyderii@gmail.com>
2018-07-06 10:12:27 -0400

    README and run_analysis.R updates

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Getting-and-Cleaning-Data-Course-Project
-Repository for the Getting and Cleaning Data Course Project.  Course is through Coursera and John Hopkins University
 
-# References:
-* Creating a factor out of two other factors: https://stat.ethz.ch/pipermail/r-help/2008-November/178808.html
+This repository contains the following files:
+
+### CodeBook.md
+
+### Getting-and-Cleaning-Data-Course-Project.Rproj
+An R project file that can used to load the porject into RStudio.  Loading this project into RStudio will ensure that the working directory is properly set and will .
+
+### README.md
+This file
+
+### run_analysis.R
+**Libraries required to run this script**
+
+* `dyplr`
+* `tidyr`
+
+This R script file runs the analysis specified in the [course project instructions](https://www.coursera.org/learn/data-cleaning/peer/FIZtT/getting-and-cleaning-data-course-project): 
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The R code that fulfills each of the above analysis requirements are tagged in the script with a comment of the form 
+
+`# **Requirement n**` 
+
+where n equals 1 to 4. 
+
+In addition, the script will download and unzip the [source data set](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) to the working directory if it is not found.
+
+
+## References
+Here are some online resources I found helpfull during the development of the run_analysis.R script
+* Create a factor out of two other factors: https://stat.ethz.ch/pipermail/r-help/2008-November/178808.html
 * Apply a function over rows of a data frame: https://stackoverflow.com/questions/7029800/how-to-run-tapply-on-multiple-columns-of-data-frame-using-r
--- run_analysis.R
+++ run_analysis.R
@@ -1,59 +1,91 @@
 library(dplyr)
 library(tidyr)
 
+#-----------------------------------------------------------------------------#
+# If the source dataset doesn't alreayd exist in the project's root
+# directory, download it.
 if(!file.exists("dataset.zip")) {
     print("Downloading dataset")
      download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
                    "dataset.zip")
 }
-print("Unzipping dataset")
-unzip("dataset.zip")
 
-# Read in activites and features from root data folder
+# Unzipping the dateset will create the UCI HAR Dataset folder in the project
+# root directory.
+if(!dir.exists("UCI HAR Dataset")) {
+    print("Unzipping dataset")
+    unzip("dataset.zip")
+}
+
+# Read in activites and features from root data folder.
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", 
                          as.is = TRUE)
 features <- read.table("UCI HAR Dataset/features.txt", 
                        col.names = c("num", "name"),  as.is = TRUE)
-features$name <- sub("\\(\\)", "", features$name)
+# remove "()" from feature names
+features$name <- sub("\\(\\)", "", features$name) 
+#-----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
 # Read in test datasets
+# read.table will replace non-alpha numeric characters in the column names 
+# with periods (".")
 test.x <- read.table("UCI HAR Dataset/test/X_test.txt", 
-                     col.names = features$name)
+                     col.names = features$name) 
 test.y <- read.table("UCI HAR Dataset/test/y_test.txt", 
                      col.names = "activity", as.is = TRUE)
 test.subjects <- read.table("UCI HAR Dataset/test/subject_test.txt", 
                             col.names = "subject", as.is = TRUE)
 test.x <- test.x[, grep("mean\\.|mean$|std", names(test.x))]
+#-----------------------------------------------------------------------------#
 
 
+#-----------------------------------------------------------------------------#
 # Read in train datasets
+# read.table will replace non-alpha numeric characters in the column names 
+# with periods (".")
+
+# **Requirement 4** Appropriate labels for the data set with descriptive 
+#                   variable names
 train.x <- read.table("UCI HAR Dataset/train/X_train.txt", 
                      col.names = features$name)
 train.y <- read.table("UCI HAR Dataset/train/y_train.txt", 
                      col.names = "activity", as.is = TRUE)
 train.subjects <- read.table("UCI HAR Dataset/train/subject_train.txt", 
                             col.names = "subject", as.is = TRUE)
+# **Requirement 2**  Extract only the measurements on the mean and the 
+#                    standard deviation for each measurement
 train.x <- train.x[, grep("mean\\.|mean$|std", names(train.x))]
+#-----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
 # Combine the test and train datasets
+# **Requirement 1**  Merge the training and the test sets to create one data set
 combined.df <- rbind(test.x, train.x)
 combined.activities <- c(test.y$activity, train.y$activity)
 combined.subjects <- c(test.subjects$subject, train.subjects$subject)
 
 # Remove train and test data
 rm(train.x, train.y, train.subjects, test.x, test.y, test.subjects)
+#-----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
 # Convert activities and subjects into factors
 combined.activities <- as.factor(combined.activities)
 combined.subjects   <- as.factor(combined.subjects)
 
-# Use descriptive names for activities
+# **Requirement 3**  Use descriptive activity names to name the activities in
+#                    the data set
 levels(combined.activities) <- activities$V2
 
 # Create a factor variable in the combined data frame that combines subjects and activites
 combined.df$factor <- interaction(combined.subjects, combined.activities)
+#-----------------------------------------------------------------------------#
 
-# Step 5: create the tidy data frame
+#-----------------------------------------------------------------------------#
+# **Requirement 5**  From the data set in step 4, creates a second, independent 
+#                    tidy data set with the average of each variable for each
+#                    activity and each subject.
 tidy.df <- combined.df %>% group_by(factor) %>% summarise_all(mean)
 
 # Separate the factor variable back into subject and activity
@@ -63,3 +95,4 @@ tidy.df$activity <- as.factor(tidy.df$activity)
 
 # Write the tidy data table to a text file
 write.table(tidy.df, file = "Step5-TidyData.txt", row.name = FALSE)
+#-----------------------------------------------------------------------------#
